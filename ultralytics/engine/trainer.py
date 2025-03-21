e58035e7c8799ec89ce8fb43ce53b5e959fa6833
Dany546 <rimez.dany@gmail.com>
2025-03-21 20:19:22 +0100

    update dataset for 3d image

--- ultralytics/engine/trainer.py
+++ ultralytics/engine/trainer.py
@@ -251,17 +251,19 @@ class BaseTrainer:
         freeze_layer_names = [f"model.{x}." for x in freeze_list] + always_freeze_names
         for k, v in self.model.named_parameters():
             # v.register_hook(lambda x: torch.nan_to_num(x))  # NaN to 0 (commented for erratic training results)
-            if any(x in k for x in freeze_layer_names): # doesn't freeze 3d conv
-                if len(v.data.shape)==5:
+            if any(x in k for x in freeze_layer_names): 
+                if len(v.data.shape)==5: # doesn't freeze 3d conv
                     v.requires_grad = True
                 else:
-                    LOGGER.info(f"Freezing layer '{k}'")
+                    # LOGGER.info(f"Freezing layer '{k}'")
                     v.requires_grad = False
             elif not v.requires_grad and v.dtype.is_floating_point:  # only floating point Tensor can require gradients
+                """ 
                 LOGGER.info(
                     f"WARNING ⚠️ setting 'requires_grad=True' for frozen layer '{k}'. "
                     "See ultralytics.engine.trainer for customization of frozen layers."
                 )
+                """
                 v.requires_grad = True
 
         # Check AMP
