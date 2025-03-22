b362e6905a52715daca5e7c6132db861de433f66
Dany546 <rimez.dany@gmail.com>
2025-03-22 23:51:07 +0100

    update dataset for 3d image

--- ultralytics/data/base.py
+++ ultralytics/data/base.py
@@ -124,7 +124,7 @@ class BaseDataset(Dataset):
         # Cache images (options are cache = True, False, None, "ram", "disk")
         self.ims, self.im_hw0, self.im_hw = [None] * self.ni, [None] * self.ni, [None] * self.ni
         self.npy_files = [Path(f).with_suffix(".npy") for f in self.im_files]
-        self.cache = cache.lower() if isinstance(cache, str) else "ram" if cache is True else None
+        self.cache = "ram" #cache.lower() if isinstance(cache, str) else "ram" if cache is True else None
         if self.cache == "ram" and self.check_cache_ram():
             if hyp.deterministic:
                 LOGGER.warning(
@@ -370,9 +370,7 @@ class BaseDataset(Dataset):
     def __getitem__(self, index):
         """Return transformed label information for given index.""" 
         s = self.imgsz
-        self.transforms[0][0].mosaic_center = (int(random.uniform(-x, 2 * s + x)) for x in (-s//2, -s//2)) 
-        print(index, self.image_ids[index][0], self.image_ids[index])
-        self.transforms(self.get_image_and_label(self.image_ids[index][0])) 
+        self.transforms[0][0].mosaic_center = (int(random.uniform(-x, 2 * s + x)) for x in (-s//2, -s//2))  
         item = [self.transforms(self.get_image_and_label(ind)) for ind in self.image_ids[index]]  
         return torch.cat(item, dim=0)
         
