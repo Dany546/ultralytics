c61be34f8042ec1fdec5487ae3003400738fed30
Dany546 <rimez.dany@gmail.com>
2025-03-26 22:23:49 +0100

    without padding

--- ultralytics/data/dataset.py
+++ ultralytics/data/dataset.py
@@ -299,7 +299,7 @@ class YOLODataset(BaseDataset):
                 value = torch.cat(value, 0)
             if k in ("im_file") and not first:
                 value = tuple(np.concatenate(value)) 
-            if k in ("ori_shape","ratio_pad") and not first:
+            if False: # k in ("ori_shape","ratio_pad") and not first:
                 print((len(value),len(value[0])), torch.cat(value, 0))
                 print(tuple(np.concatenate(value)) , torch.cat(value, 0))
                 value = torch.cat(value, 0)   
