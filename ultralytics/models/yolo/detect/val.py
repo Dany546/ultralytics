13fa818a5708b366680d5e5cc2011e9e7c9f4eeb
Dany546 <rimez.dany@gmail.com>
2025-03-27 20:30:22 +0100

    correct plot_images

--- ultralytics/models/yolo/detect/val.py
+++ ultralytics/models/yolo/detect/val.py
@@ -353,7 +353,7 @@ class DetectionValidator(BaseValidator):
             batch (dict): Batch containing images and annotations.
             ni (int): Batch index.
         """
-        print(batch["bboxes"].shape, batch["bboxes"])
+        print(batch["batch_idx"], batch["img"].shape, batch["bboxes"].shape, batch["bboxes"])
         plot_images(
             batch["img"],
             batch["batch_idx"],
